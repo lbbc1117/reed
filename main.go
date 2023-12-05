2778310a679683b94934b0d9d942313f45d10632
heying <heying@jushuitan.com>
2023-12-05 18:05:10 +0800

    cmd line

--- main.go
+++ main.go
@@ -13,44 +13,48 @@ func main() {
 		EnableBashCompletion: true,
 		Commands: []*cli.Command{
 			{
-				Name:    "add",
-				Aliases: []string{"a"},
-				Usage:   "add a task to the list",
+				Name:  "gen",
+				Usage: "generate models and mongodb api",
 				Action: func(cCtx *cli.Context) error {
-					fmt.Println("added task: ", cCtx.Args().First())
+					fmt.Println(cCtx.Args())
+					fmt.Println("added task: ", cCtx.String("add"))
 					return nil
+				},
+				Flags: []cli.Flag{
+					&cli.PathFlag{
+						Name:     "json",
+						Aliases:  []string{"j"},
+						Required: true,
+						Usage:    "path to the *.json file which contains model templates",
+					},
+					&cli.PathFlag{
+						Name:    "dir",
+						Aliases: []string{"d"},
+						Value:   "./",
+						Usage:   "generated code will be located in this dir",
+					},
+					&cli.StringSliceFlag{
+						Name:    "add",
+						Aliases: []string{"a"},
+						Usage:   "add global field in models, format: <name>:<type>",
+					},
 				},
 			},
 			{
-				Name:    "complete",
-				Aliases: []string{"c"},
-				Usage:   "complete a task on the list",
+				Name:  "types",
+				Usage: "list available global field types mapping from reed to Go",
 				Action: func(cCtx *cli.Context) error {
-					fmt.Println("completed task: ", cCtx.Args().First())
+					fmt.Println("\nAvailable global field types:\t")
+					fmt.Println("--------------------------\t")
+					fmt.Println("reed\t", "\t", "Go")
+					fmt.Println("--------------------------\t")
+					fmt.Println("int\t", "--->\t", "int64")
+					fmt.Println("float\t", "--->\t", "float64")
+					fmt.Println("time\t", "--->\t", "time.Time")
+					fmt.Println("string\t", "--->\t", "string")
+					fmt.Println("bool\t", "--->\t", "bool")
+					fmt.Println("--------------------------\t")
 					return nil
-				},
-			},
-			{
-				Name:    "template",
-				Aliases: []string{"t"},
-				Usage:   "options for task templates",
-				Subcommands: []*cli.Command{
-					{
-						Name:  "add",
-						Usage: "add a new template",
-						Action: func(cCtx *cli.Context) error {
-							fmt.Println("new task template: ", cCtx.Args().First())
-							return nil
-						},
-					},
-					{
-						Name:  "remove",
-						Usage: "remove an existing template",
-						Action: func(cCtx *cli.Context) error {
-							fmt.Println("removed task template: ", cCtx.Args().First())
-							return nil
-						},
-					},
 				},
 			},
 		},
